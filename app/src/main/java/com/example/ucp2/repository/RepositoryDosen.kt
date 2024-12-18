29a967898176599c1dfbe2d193804de9da2e03eb
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-18 10:55:33 +0700

    Membuat RepositoryDosen

---
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDosen {
+
+    // Dosen-related methods
+    suspend fun insertDosen(dosen: Dosen)
+
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    fun getDosen(nidn: String): Flow<Dosen>
+
+}
