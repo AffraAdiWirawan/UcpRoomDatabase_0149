711168043333721a6fccf1f1a5c9db832f887efb
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-23 14:12:21 +0700

    Membuat AlamatNavigasi

---
+++ app/src/main/java/com/example/ucp2/ui/navigasi/AlamatNavigasi.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2.ui.navigasi
+
+interface AlamatNavigasi {
+    val route: String
+
+    object DestinasiHomeApp: AlamatNavigasi{
+        override val route = "homeapp"
+    }
+
+    object DestinasiHomeDosen: AlamatNavigasi {
+        override val route = "home"
+    }
+
+    object DestinasiHomeMK: AlamatNavigasi {
+        override val route = "homemk"
+    }
+
+    object DestinasiDetailMK: AlamatNavigasi {
+        override val route = "detailmk"
+        const val Kode = "kode"
+        val routeWithArg = "$route/{$Kode}"
+    }
+
+    object DestinasiUpdateMK: AlamatNavigasi {
+        override val route = "updatemk"
+        const val Kode = "kode"
+        val routeWithArg = "$route/{$Kode}"
+    }
+
+}
