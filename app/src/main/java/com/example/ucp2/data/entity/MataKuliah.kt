3998469a9615f730947776b82727119d5c7b85b1
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2024-12-23 14:29:36 +0700

    Commit Error

--- app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
+++ app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
@@ -3,7 +3,7 @@ package com.example.ucp2.data.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "mataKuliah")
+@Entity(tableName = "matakuliah")
 data class MataKuliah(
     @PrimaryKey
     val kode: String,
